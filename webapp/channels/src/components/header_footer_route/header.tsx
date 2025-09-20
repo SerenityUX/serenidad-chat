b667590e9038678d711e69b53bfbd3a8fb3f4ea1
serenityux <thomas@serenidad.app>
2025-09-20 00:11:56 -0700

    updating header

--- webapp/channels/src/components/header_footer_route/header.tsx
+++ webapp/channels/src/components/header_footer_route/header.tsx
@@ -6,13 +6,12 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
 
-import {getConfig, getLicense} from 'mattermost-redux/selectors/entities/general';
+import {getConfig} from 'mattermost-redux/selectors/entities/general';
 
 import BackButton from 'components/common/back_button';
-import Logo from 'components/common/svg_images_components/logo_dark_blue_svg';
+import Logo from 'images/logo.svg';
 
 import './header.scss';
-import {LicenseSkus} from 'utils/constants';
 
 export type HeaderProps = {
     alternateLink?: React.ReactElement;
@@ -22,39 +21,18 @@ export type HeaderProps = {
 
 const Header = ({alternateLink, backButtonURL, onBackButtonClick}: HeaderProps) => {
     const {SiteName} = useSelector(getConfig);
-    const license = useSelector(getLicense);
 
     const ariaLabel = SiteName || 'Mattermost';
 
-    let freeBanner = null;
-    if (license.IsLicensed === 'false') {
-        freeBanner = <><Logo/><span className='freeBadge'>{'TEAM EDITION'}</span></>;
-    } else if (license.SkuShortName === LicenseSkus.Entry) {
-        freeBanner = <><Logo/><span className='freeBadge'>{'ENTRY EDITION'}</span></>;
-    }
-
     let title: React.ReactNode = SiteName;
     if (title === 'Mattermost') {
-        if (freeBanner) {
-            title = '';
-        } else {
-            title = <Logo/>;
-        }
+        title = <img src={Logo} alt="Logo" style={{width: '116px', height: '20px', filter: 'brightness(0) saturate(100%) invert(27%) sepia(51%) saturate(2000%) hue-rotate(200deg) brightness(0.75)'}} />;
     }
 
     return (
-        <div className={classNames('hfroute-header', {'has-free-banner': freeBanner, 'has-custom-site-name': title})}>
+        <div className={classNames('hfroute-header', {'has-custom-site-name': title})}>
             <div className='header-main'>
                 <div>
-                    {freeBanner &&
-                        <Link
-                            className='header-logo-link'
-                            to='/'
-                            aria-label={ariaLabel}
-                        >
-                            {freeBanner}
-                        </Link>
-                    }
                     {title &&
                         <Link
                             className='header-logo-link'
